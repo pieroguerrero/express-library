494eeef7d38536e9b5bbc6f4c618d53ceb9df09e
Piero <piero.guerrero@gmail.com>
2022-11-04 11:51:08 -0500

    Book instance creation

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -1,7 +1,8 @@
-// Display list of all BookInstances.
-
+import { body, validationResult } from "express-validator";
 import BookInstance from "../models/bookinstance.js";
+import Book from "../models/book.js";
 
+// Display list of all BookInstances.
 const bookinstance_list = (req, res, next) => {
   const findPromise = BookInstance.find().populate("book").exec();
 
@@ -38,14 +39,72 @@ const bookinstance_detail = (req, res, next) => {
 };
 
 // Display BookInstance create form on GET.
-const bookinstance_create_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance create GET");
+const bookinstance_create_get = (req, res, next) => {
+  Book.find({}, "title")
+    .exec()
+    .then((books) => {
+      res.render("bookinstance_form", {
+        title: "Create BookInstance",
+        book_list: books,
+      });
+    })
+    .catch((err) => next(err));
 };
 
 // Handle BookInstance create on POST.
-const bookinstance_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance create POST");
-};
+const bookinstance_create_post = [
+  // Validate and sanitize fields.
+  body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+  body("imprint", "Imprint must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status").escape(),
+  body("due_back", "Invalid date")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a BookInstance object with escaped and trimmed data.
+    const bookinstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    });
+
+    if (!errors.isEmpty) {
+      // There are errors. Render form again with sanitized values and error messages.
+
+      Book.find({}, "title")
+        .exec()
+        .then((books) => {
+          res.render("bookinstance_form", {
+            title: "Create BookInstance",
+            book_list: books,
+            selected_book: bookinstance.book._id,
+            errors: errors.array(),
+            bookinstance,
+          });
+        })
+        .catch((err) => next(err));
+
+      return;
+    }
+
+    bookinstance
+      .save()
+      .then(() => {
+        // @ts-ignore
+        res.redirect(bookinstance.url);
+      })
+      .catch((err) => next(err));
+  },
+];
 
 // Display BookInstance delete form on GET.
 const bookinstance_delete_get = (req, res) => {
